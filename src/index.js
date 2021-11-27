bc7a3c81d8ce9cbb5246fdc81418a936debe7f14
knightfury1493 <suhaibahmmad1493@gmail.com>
2021-11-27 16:29:01 +0600

    connected the setup form with the backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
-console.log("From index.js");
+import {initializeApp} from 'firebase/app';
+import {getAuth , createUserWithEmailAndPassword} from 'firebase/auth';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAxzX4sBmzREWTUizcb5p6DAibIi2djfWU",
+    authDomain: "net-ninja-firebase-7e164.firebaseapp.com",
+    projectId: "net-ninja-firebase-7e164",
+    storageBucket: "net-ninja-firebase-7e164.appspot.com",
+    messagingSenderId: "559767507773",
+    appId: "1:559767507773:web:9b8cb4a842fd1a4d6eeb54",
+    measurementId: "G-QC7Z205B2K"
+  };
+
+
+//   init app
+
+initializeApp(firebaseConfig);
+
+// init service
+const auth = getAuth(); 
+
+
+
+
+//sign-up
+
+const signupForm = document.querySelector("#signup-form");
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+
+   // get user info
+    let email = document.querySelector("#signup-email").value;
+    let password = document.querySelector("#signup-password").value;
+    let confirmPassword = document.querySelector('#confirm-password').value;
+
+    if(password != confirmPassword)
+    {
+        console.log("Password did not match");
+    }
+
+    else{
+
+        createUserWithEmailAndPassword(auth,email,password).then( cred => {
+            console.log(cred.user);
+        }).catch( err => {
+            console.log(err.message);
+        });
+    }
+
+    email = "";
+    password = "";
+    confirmPassword = "";
+
+})
+
+
